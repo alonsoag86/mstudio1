395a305455b43e88a8a07bec2dcaed40475a37e0
EC2 Default User <ec2-user@ip-172-31-41-252.ec2.internal>
2018-11-13 16:28:40 +0000

    improved layout

--- un_project/sketch.js
+++ un_project/sketch.js
@@ -2,13 +2,13 @@
 
 console.log('hello world')
 
-let ssa = [" Sub-Saharan Africa", " Angola", " Benin", " Botswana", " Burkina Faso", " Burundi", " Cabo Verde", " Cameroon", " Central African Republic", " Chad", " Comoros", " Congo", " Democratic Republic of the Congo", " Cote d'Ivoire", " Equatorial Guinea", " Eritrea", " Ethiopia", " Gabon", " Gambia", " Ghana", " Guinea", " Guinea-Bissau", " Kenya", " Lesotho", " Liberia", " Madagascar", " Malawi", " Mali", " Mauritania", " Mauritius", " Mozambique", " Niger", " Nigeria", " Rwanda", " Sao Tome and Principe", " Senegal", " Seychelles", " Sierra Leone", " South Africa", " South Sudan", " Swaziland", " United Republic of Tanzania", " Togo", " Uganda", " Zambia", " Zimbabwe"];
+let ssa = [" Angola", " Benin", " Botswana", " Burkina Faso", " Burundi", " Cabo Verde", " Cameroon", " Central African Republic", " Chad", " Comoros", " Congo", " Democratic Republic of the Congo", " Cote d'Ivoire", "Djibouti", "Equatorial Guinea", " Eritrea", " Ethiopia", " Gabon", " Gambia", " Ghana", " Guinea", " Guinea-Bissau", " Kenya", " Lesotho", " Liberia", " Madagascar", " Malawi", " Mali", " Mauritania", " Mauritius", " Mozambique", " Niger", " Nigeria", " Rwanda", " Sao Tome and Principe", " Senegal", " Seychelles", " Sierra Leone", "Somalia", " South Africa", " South Sudan", " Sudan", " United Republic of Tanzania", " Togo", " Uganda", " Zambia", " Zimbabwe"];
+//console.log(ssa.length)
+//let year_2010 = '2010';
+//let year_2015 = '2015';
 
-let year_2010 = '2010';
-let year_2015 = '2015';
 
-
-let selectCountry = d3.select('body')
+/*let selectCountry = d3.select('body')
     .append('select')
     .on('change', update)
     .selectAll('option')
@@ -18,8 +18,19 @@ let selectCountry = d3.select('body')
     .text(function (d) { return d 
         
     });
+    */
     
-let wrapper_1 = d3.select('body')
+let selectCountry = d3.select('select')
+    .on('change', update)
+    .selectAll('option')
+    .data(ssa)
+    .enter()
+    .append('option')
+    .text(function (d) { return d 
+        
+    });
+    
+/*let wrapper_1 = d3.select('body')
     .append('div')
     .attr('id', 'wrapper1')
     
@@ -75,10 +86,30 @@ for (let i = 1; i < 6; i++) {
     .text('hello')
     
 }   
+*/
          
+let t = function(){ return d3.transition().duration(1000); }
+
+
+d3.json("data.json").then(function(data){
+    console.log(data);
+    nested_data = {}
+    nested_data = d3.nest()
+    .key(function(d) { return d.country; })
+    .entries(data)
+    console.log(nested_data);
+
+    update();
+
+});
            
-    function update(){
-        let updateVal = d3.select(selectCountry)
-            //console.log(this.value)
-            .property(this.value)
-    }
+function update(){
+   
+  let country = $("select").val()
+  console.log(country);
+  let countryFiltered = nested_data[country].filter(function(d){
+        return countryFiltered
+        console.log(countryFiltered)
+    });
+   
+}
